6fb15c6f562561681d8b4bbcd3cc16ff7b409a6e
rdavid66 <mmeta666@yahoo.com>
2019-05-07 20:25:27 +0200

    Update sketch.ino
    maybe

--- sketch.ino
+++ sketch.ino
@@ -1,22 +1,17 @@
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
-
-  https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-*/
-
 const int rightPinNum = 2;
 const int leftPinNum = 3;
+const int diffFrac = 2;
+const int allDifFrac = 2;
 
 volatile unsigned rightRevs = 0;
 volatile unsigned leftRevs = 0;
+
+const int speedLeft = 150;
+int speedRight = 150;
+
+int allDiff = 0;
+
+int speed = speedRight;
 
 void onRightRev() { ++rightRevs; }
 void onLeftRev() { ++leftRevs; }
@@ -24,11 +19,6 @@ void onLeftRev() { ++leftRevs; }
 int clamp(int min, int max, int value) {
   return value < min ? min : ( value > max ? max : value );  
 }
-
-const int speedLeft = 200;
-int speedRight = 200;
-
-int allDiff = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -44,52 +34,49 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-
-  // A0 middle, A1 right, A2 left
-  
-  int sensorValue0 = analogRead(A0);
-  int sensorValue1 = analogRead(A1);
-  int sensorValue2 = analogRead(A2);
-
   Serial.print("right: ");
   Serial.println(rightRevs);
   Serial.print("left: ");
   Serial.println(leftRevs);
 
-  // print out the value you read:
-  Serial.print("A0: ");
-  Serial.println(sensorValue0);
-  Serial.print("A1: ");
-  Serial.println(sensorValue1);
-  Serial.print("A2: ");
-  Serial.println(sensorValue2);
+  int left = leftRevs;
+  int right = rightRevs;
+  leftRevs = rightRevs = 0;  
+  int diff = left - right;
+  right = (right/speed) * speedRight;  
+  allDiff += diff;
 
-  int speed = 0;
+  Serial.print("All Diff: ");
+  Serial.println(allDiff);
+
+  diff = left - right;
+  
+  Serial.print("Diff: ");
+  Serial.println(diff);
+
+  speedRight = clamp(0, 255, speedRight - diff/diffFrac);
+  speed = clamp(0, 255, speedRight - allDiff/allDifFrac);
+  //digitalWrite(8, true);
+  analogWrite(9, speed);
+  //digitalWrite(10, false);
+  analogWrite(11, speedLeft);
+  delay(100);        // delay in between reads for stability
+
+ // A0 middle, A1 right, A2 left
+ // int sensorValue0 = analogRead(A0);
+ // int sensorValue1 = analogRead(A1);
+ // int sensorValue2 = analogRead(A2);
+ // print out the value you read:
+ // Serial.print("A0: ");
+ // Serial.println(sensorValue0);
+ // Serial.print("A1: ");
+ // Serial.println(sensorValue1);
+ // Serial.print("A2: ");
+ // Serial.println(sensorValue2);
 
   // digital write to 8: right reverse
   //                 10: left reverse
 
   // analog write to 9: right speed
   //                 11: left speed
-
-  int diff = leftRevs - rightRevs;
-
-  leftRevs = rightRevs = 0;
-
-  Serial.print("Diff: ");
-  Serial.println(diff);
-
-  allDiff += diff;
-
-  Serial.print("All Diff: ");
-  Serial.println(allDiff);
-
-  speedRight = clamp(0, 255, speedRight - diff - allDiff);
-  
-  //digitalWrite(8, true);
-  analogWrite(9, speedRight);
-  //digitalWrite(10, false);
-  analogWrite(11, speedLeft);
-  delay(25);        // delay in between reads for stability
 }
